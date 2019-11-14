469931e836fac09869b6ffd390c1a2b1ce89e2ac
Bryant Fiallos <bryant.fiallos@gmail.com>
2019-11-14 10:36:36 -0500

    commit

---
+++ src/reverse.js
@@ -0,0 +1,17 @@
+const readlineSync = require("readline-sync")
+
+let reversed = 0;
+let number = Number(readlineSync.question("\nPositive integer: "));
+
+while (number < 1 || number > Number.MAX_SAFE_INTEGER || Number.isNaN(number) || !Number.isInteger(number)) {
+  number = Number(readlineSync.question("Positive integer: "))
+}
+
+ while (number != 0) {
+  reversed *= 10;
+  reversed += number % 10;
+  number -= number % 10;
+  number /= 10;
+ }
+
+ console.log(reversed);
