042e8b5c6e5150d8b14f86d3b853e44c7cc9bbdf
Bryant Fiallos <bryant.fiallos@gmail.com>
2019-11-18 10:28:09 -0500

    odd

---
+++ src/odd.js
@@ -0,0 +1,16 @@
+const readlineSync = require("readline-sync");
+
+let reversed = "";
+let digit = 0;
+let number = Number(readlineSync.question("\nPositive integer: "));
+
+while (number < 1 || number > Number.MAX_SAFE_INTEGER || Number.isNaN(number) || !Number.isInteger(number)) {
+  number = Number(readlineSync.question("Positive integer: "))
+}
+
+while (number > 10) {
+  digit = number % 10;
+  reversed = reversed + digit + ", ";
+  number -= number % 10;
+  number /= 10;
+}
