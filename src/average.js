274328d00d8b514172b00fbe21d73d09a7b9b1fd
Bryant Fiallos <bryant.fiallos@gmail.com>
2019-11-20 10:53:17 -0500

    commit

---
+++ src/average.js
@@ -0,0 +1,20 @@
+const readlineSync = require("readline-sync");
+
+let sum = 0;
+let value = 0;
+let divisor = 0;
+
+
+do {
+  value = Number(readlineSync.question("Non-negative integer: "));
+  divisor = divisor + 1;
+  if (value > 0) {
+    sum = sum + value;
+  }
+}
+while (value > 0)
+
+let mean = sum / (divisor - 1)
+
+let average = mean.toLocaleString("en", { minimumFractionDigits: 0, maximumFractionDigits: 3});
+console.log(average);
