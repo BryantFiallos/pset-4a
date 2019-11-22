63442ec6569c2807e21befe0222793af0ab7cadf
Bryant Fiallos <bryant.fiallos@gmail.com>
2019-11-22 10:18:40 -0500

    prime

---
+++ src/prime.js
@@ -0,0 +1,23 @@
+const readlineSync = require("readline-sync");
+
+let integer = 0;
+let prime = "";
+console.log()
+
+do {
+  integer = Number(readlineSync.question("Non-negative integer: "))
+}
+while (integer <= 0 || Number.isNaN(integer) || integer > Number.MAX_SAFE_INTEGER|| !Number.isInteger(integer))
+
+for (var x = 2; x < integer; x++) {
+  if (integer % x == 0) {
+    prime = "no";
+    break;
+  }
+}
+if (prime == "no") {
+    console.log("\nNot prime.")
+}
+else {
+  console.log("\nPrime.")
+}
