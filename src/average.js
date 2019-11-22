8ccb1df1370e4158bca5d313a2318f8bb25b1d3a
unknown <bryant.fiallos@gmail.com>
2019-11-21 19:50:23 -0500

    maybe finished average

--- src/average.js
+++ src/average.js
@@ -1,20 +1,22 @@
 const readlineSync = require("readline-sync");
 
 let sum = 0;
-let value = 0;
+let integer = 0;
 let divisor = 0;
 
+console.log()
 
 do {
-  value = Number(readlineSync.question("Non-negative integer: "));
-  divisor = divisor + 1;
-  if (value >= 0) {
-    sum = sum + value;
+  integer = Number(readlineSync.question("Non-negative integer: "));
+
+  if (integer >= 0 && integer < Number.MAX_SAFE_INTEGER && !Number.isNaN(integer) && integer % 1 == 0 && Number.isInteger(integer)) {
+    sum = sum + integer;
+    divisor = divisor + 1;
   }
 }
-while (value >= 0)
+while ((integer >= 0) || Number.isNaN(integer) || divisor == 0)
 
-let mean = sum / (divisor - 1)
+let mean = sum / divisor
 
 let average = mean.toLocaleString("en", { minimumFractionDigits: 0, maximumFractionDigits: 3});
-console.log(average);
+console.log("\n" + average + ".");
